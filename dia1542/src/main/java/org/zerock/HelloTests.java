52b2608a3876a3b611d322cc3e4d5c346adbc77e
Yekapark <qkrwkddjs777@gmail.com>
2019-10-18 18:14:17 +0900

    test

---
+++ dia1542/src/main/java/org/zerock/HelloTests.java
@@ -0,0 +1,25 @@
+package org.zerock;
+
+import org.junit.Test;
+
+/**
+ * HelloTests
+ */
+public class HelloTests {
+
+    @Test
+    public void test1(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
+
+// db서버
+
+// 공통계정
+
+// sqldeveloper 엑세스
+
+// cputil -> 지금 db로 바꿔서 연결테스트.
